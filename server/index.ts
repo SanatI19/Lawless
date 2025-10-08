bde8dda80b7d8ebf42bc9f499327d09c6ba99a50
Sanat Iyer <sanat.iyer19@gmail.com>
2025-10-08 18:26:23 -0500

    almost

--- server/index.ts
+++ server/index.ts
@@ -32,8 +32,8 @@ const io = new Server<ClientToServerEvents,ServerToClientEvents>(server, {
 
 });
 
-
-server.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log("Server listening on port " + (process.env.PORT || 3000));
 });
 
